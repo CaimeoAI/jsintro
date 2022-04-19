4bf816e572a871a57b45591b9251b928247a7bde
Judielle Villa <judiellezinke@yahoo.de>
2022-04-19 13:34:32 +0200

    updated

---
+++ 05operators.js
@@ -0,0 +1,25 @@
+//! In JS we have 3 kinds of operators
+
+//? arithmetic operator
+//? conditional operator
+//? logical operator
+
+//TODO Arithmetic Operator
+// + - * / % modulos ** exponential
+
+console.log(2+2); // 4
+console.log(3-1); // 2
+console.log(4*3); // 12
+console.log(10/5); // 2
+console.log(10%3); // 1 [outouts the remainder]
+console.log(2**3); // 2*2*2 = 8
+
+// example with a variable
+
+let a = 2;
+// post increment operator [outputs starting value and adds 1 to it's stored value afterwards]
+console.log(a++);
+console.log(a);
+// pre increment operator [Adds 1 to it's stored value and then output the new value]
+console.log(++a);
+console.log(a);
