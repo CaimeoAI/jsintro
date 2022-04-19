d690f43eae266f1fc801c62d6363bf1f5d334eed
Judielle Villa <judiellezinke@yahoo.de>
2022-04-19 12:02:12 +0200

    updated

---
+++ 04String.js
@@ -0,0 +1,9 @@
+let firstName = "Misbah"; //string
+let age = 23; //number
+let isStudent = true; //boolean
+
+// Concatenation
+
+console.log ("con" + "cati" + "nation");
+
+console.log("My name is " + firstName + " and I am " + age + " years old.");
