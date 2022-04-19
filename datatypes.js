5c6690853fc75cc866f70f8317e3a45d3c11ed94
Judielle Villa <judiellezinke@yahoo.de>
2022-04-19 11:48:41 +0200

    updated

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+//* Data : Informmation / anything which is meaningful to a machine is called data
+
+//? Data Types in JS
+//? There are two main types in JS
+
+//? Primitive
+//? Object
+
+//! Primitive Type
+// String [Textual Data within the Quotation Marks/ Backticks]
+//TODO "Hello World" '123' `Neo9826`
+
+// Number [Numbers that can be used for calculations by the machine]
+//? Integer(full numbers), float(decimal numbers), + infinity(10/3 or Pi), - Infinity, NaN
+
+// undefined [Empty Value, When you declare a Variable without]
+//TODO let var0 = ;
+
+// null [When you set empty Value to Variable]
+//TODO let var0 = null;
+
+// symbol
+
+
+// Boolean [True or False]
+//TODO let var1 = false;
+
+//! Object
+// Array
+// object
