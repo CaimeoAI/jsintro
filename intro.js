539bb8eaf96153480f8192e68ce668974ff2957f
Judielle Villa <judiellezinke@yahoo.de>
2022-04-19 11:15:46 +0200

    Initial commit

---
+++ intro.js
@@ -0,0 +1,33 @@
+//* Single Line Comment
+
+/*
+Multi Line
+Comment
+*/
+
+//* Runtime Environments for JS
+//? Browser
+//? NODEJS
+
+//* REPL
+//? Read Evaluate Print Loop
+
+//? () Paranthesis
+//? {} Curly Braces/Brackets
+//? [] Square Brackets
+
+//? "" Double Quotes/Quotion
+//? '' Single Quotions
+//? `` Back Ticks
+
+//? ; semicolon
+//? : colon
+
+//* Write our first hello world program
+
+//! console.log will print argument in the console
+console.log(1+1); //! Instruction to machine to print hello world on the screen (STATEMENT)
+
+//* Program
+//? Set of instructions to perform any action/task
+
---
+++ variables.js
@@ -0,0 +1,29 @@
+//* Variable
+//? Variables are like a container holding value withing the memory
+
+//! Three Different ways to define variables
+
+//? var firstName = "John"; (avoid using var)
+//? let firstName = "John";
+//? const firstName = "John";
+
+//! Execution of code in js is from top to bottom and left to right
+
+// var lastName = "Smith";
+
+// console.log(lastName);
+
+// var lastName = "Naqvi";
+
+// console.log(lastName);
+
+
+//! let will not allow you to redefine your variable, but you can overwrite it
+//? Defining a Variable = Assignment Operator 
+
+let lastName = "Smith";
+
+lastName = "John";
+
+console.log(lastName)
+
